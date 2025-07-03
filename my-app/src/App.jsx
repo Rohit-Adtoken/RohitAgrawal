8b2fda2e95552651fd4469dfd60886d1089eb287
Rohit AT <rohit@adtoken.co>
2025-07-03 21:28:00 +0530

    initial commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-indigo-500 to-purple-600 text-white">
+      <h1 className="text-4xl bg-red-200 font-bold">🚀 Tailwind with Vite is Ready!</h1>
+    </div>
+  );
+}
+
+export default App;
